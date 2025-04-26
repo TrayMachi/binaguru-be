12d87e12fa892f493bd543b05f98ddbdf2ca8535
TrayMachi <tristan.agra@ui.ac.id>
2025-04-26 19:27:17 +0700

    feat: init prisma module

---
+++ prisma/migrations/20250426122638_user_table/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "yoe" INTEGER NOT NULL,
+    "pros" TEXT NOT NULL,
+    "cons" TEXT NOT NULL,
+    "location" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
